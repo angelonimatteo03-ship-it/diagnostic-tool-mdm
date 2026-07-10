b567d397a119421ab8a3450b062ea1d21626a507
angelonimatteo03-ship-it <angeloni.matteo03@gmail.com>
2026-07-10 11:55:35 +0200

    Footer component

---
+++ components/common/Footer.tsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <footer className="bg-mdm-dark text-white py-12 mt-20">
+      <div className="max-w-7xl mx-auto px-4 text-center">
+        <p className="text-mdm-light/70">
+          © 2024 Maisons du Monde - Interior Design Diagnostic Tool
+        </p>
+        <p className="text-sm text-mdm-light/50 mt-2">
+          Uso interno esclusivo
+        </p>
+      </div>
+    </footer>
+  )
+}
